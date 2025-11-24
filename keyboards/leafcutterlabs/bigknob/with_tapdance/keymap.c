3a555b9be32229d834078a7cd6895df590f4a3a2
talosgl_win11_twr <68163715+talosgl@users.noreply.github.com>
2025-11-23 22:49:26 -0800

    Add keymap with tapdance added back in based on commit b7b82d509bfccd69c6507761f03910f34b4366cb

---
+++ keyboards/leafcutterlabs/bigknob/with_tapdance/keymap.c
@@ -0,0 +1,57 @@
+/* Copyright 2021 Craig Gardner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+#define _MAIN 0
+
+// -------- knob
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        // Normal: volume control
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    return true;
+}
+
+// -------- RGB tapdance stuff
+enum {
+    TD_RGB = 0,
+};
+
+void dance_rgb_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        tap_code(KC_MNXT);
+    } else if (state->count == 2) {
+        rgblight_toggle();
+    } else if (state->count == 3) {
+        rgblight_step();
+    }
+}
+
+// All tap dance functions
+tap_dance_action_t tap_dance_actions[] = {[TD_RGB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_rgb_finished, NULL)};
+
+// -------- actually set the keymap
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // button closest to USB is first
+    [_MAIN] = LAYOUT(KC_MUTE, KC_MPRV, KC_MPLY, KC_MSTP,
+                     TD(TD_RGB) // Single tap = Next track, Double tap = RGB toggle, Triple tap = Cycle mode
+                     )};
